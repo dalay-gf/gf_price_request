62c5dd825fc86ca675c15708e9abf3705f60016f
dalay <dalay@drupalka.ru>
2019-04-09 01:34:21 +0300

    js && css

---
+++ gf_price_request.js
@@ -0,0 +1,34 @@
+(function ($) {
+    Drupal.behaviors.gfPriceRequest = {
+        attach: function (context, settings) {
+            var priceForm = $('form.gf_price_request_form');
+            var priceBox = $('.price-box', priceForm);
+            var currentPercent = 0;
+            var currentPrice = priceBox.text();
+            $('.gf-price-request', priceForm).click(function() {
+                var percentBox = $(this).prevAll('.current-percent:first');
+
+                $(this).siblings('input.new-price-submit-button').removeClass('d-none');
+                if ($(this).hasClass('plus')){
+                    currentPercent += 1;
+                } else if ($(this).hasClass('minus')) {
+                    currentPercent -= 1;
+                }
+                if (currentPercent >=0) {
+                    percentBox.removeClass('minus').addClass('plus');
+                } else {
+                    percentBox.removeClass('plus').addClass('minus');
+                }
+                
+                var newPrice = +currentPrice + (currentPrice * (currentPercent / 100));
+                newPrice = Math.round(newPrice)
+
+                percentBox.html(currentPercent + '%');
+                priceBox.text(newPrice);
+                $('input[name="gf_price_request_new_price"]', priceForm).val(newPrice);
+                $('input[name="gf_price_request_percent"]', priceForm).val(currentPercent);
+            });
+        }
+    };
+})(jQuery);
+
