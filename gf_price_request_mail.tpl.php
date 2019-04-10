aa561de918043c46ba983c8186c477dc557f9e3f
dalay <dalay@drupalka.ru>
2019-04-10 19:06:36 +0300

    changes in mail template

--- gf_price_request_mail.tpl.php
+++ gf_price_request_mail.tpl.php
@@ -1,20 +1,20 @@
 <p>
-<?php print t('From site giorgio-ferretti.it received a request to change the price of goods.'); ?>
+С сайта giorgio-ferretti.it поступил запрос на изменение цены товара.
 </p>
 <ul>
   <li>
-  <strong><?php print t('Model:'); ?></strong> <?php print l($node->model, 'node/' . $node->nid); ?>
+  <strong>Модель товара: </strong> <?php print l($node->model, 'node/' . $node->nid); ?>
   </li>
   <li>
-  <strong><?php print t('Percent:'); ?></strong> <?php print $percent; ?>
+  <strong>Процент (изменения относительно текущей цены):</strong> <?php print $percent; ?>%
   </li>
   <li>
-  <strong><?php print t('Price Region:'); ?></strong> <?php print $current_region; ?>
+  <strong>Регион (склад, валюта):</strong> <?php print $current_region; ?>
   </li>
   <li>
-  <strong><?php print t('Current Price:'); ?></strong> <?php print $current_price; ?>
+  <strong>Текущая цена товара (на региональном складе):</strong> <?php print $current_price; ?>
   </li>
   <li>
-  <strong><?php print t('New Price:'); ?></strong> <?php print $new_price; ?>
+  <strong>Новая цена товара:</strong> <?php print $new_price; ?>
   </li>
 </ul>
